0f5555efb77ac7424006997a3b1aac329142c052
chadjaa sofiane <sofianne012@gmail.com>
2022-01-02 20:17:27 +0100

    initialise the app, add webpack, babel, eslint

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <h1> are kidding me sofiane, you are amazing </h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { join } = require("path");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  devtool: !isProduction ? "source-map" : false,
+  mode: isProduction ? "production" : "development",
+  target: "web",
+  entry: "./src/index.jsx",
+  output: {
+    // eslint-disable-next-line no-undef
+    path: join(__dirname, "dist"),
+    filename: "bundle.[hash].js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".jsx", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_module/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /.css$/,
+        exclude: /node_module/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              module: true,
+              localsConvention: "camelCase",
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+  },
+};
