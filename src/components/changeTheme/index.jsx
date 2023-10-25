705ec9deb71a7edb4ae1277964864409e28827b6
chadjaa-sofiane <sofianne012@gmail.com>
2023-10-25 08:20:12 +0100

    add the circle background and its style.

--- src/components/changeTheme/index.jsx
+++ src/components/changeTheme/index.jsx
@@ -1,16 +1,28 @@
 import React, { useContext } from "react";
-import { ChangeThemeContainer } from "./style";
+import { ChangeThemeWrapper, ChangeThemeContainer, ThemeCircle } from "./style";
 import SunIcon from "../../assets/svgs/sun.svg";
 import MoonIcon from "../../assets/svgs/moon.svg";
 import { AppContext } from "../../App";
+import { createPortal } from "react-dom";
 
 const ChangeTheme = () => {
   const { switchThemes, theme } = useContext(AppContext);
+
   return (
+    <ChangeThemeWrapper>
+      <ChangeThemIcons theme={theme} switchThemes={switchThemes} />
+      <ThemeCircle themeType={theme} />
+    </ChangeThemeWrapper>
+  );
+};
+
+const ChangeThemIcons = ({ theme, switchThemes }) => {
+  return createPortal(
     <ChangeThemeContainer onClick={switchThemes}>
       {theme === "light" && <SunIcon />}
       {theme === "dark" && <MoonIcon />}
-    </ChangeThemeContainer>
+    </ChangeThemeContainer>,
+    document.getElementById("portal")
   );
 };
 
--- src/components/changeTheme/style.jsx
+++ src/components/changeTheme/style.jsx
@@ -1,5 +1,19 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
+const ligthCercleAnimation = keyframes`
+ from{
+  scale: 50;
+ }
+ to {
+  scale: 0;
+ }
+`;
+export const ChangeThemeWrapper = styled.div`
+  position: fixed;
+  left: 1em;
+  bottom: 1em;
+  z-index: -1;
+`;
 export const ChangeThemeContainer = styled.div`
   position: fixed;
   width: 50px;
@@ -7,6 +21,7 @@ export const ChangeThemeContainer = styled.div`
   bottom: 1em;
   left: 1em;
   display: flex;
+  z-index: 1;
   cursor: pointer;
   svg {
     width: 100%;
@@ -21,4 +36,29 @@ export const ChangeThemeContainer = styled.div`
       opacity: 1;
     }
   }
+`;
+
+const ThemeAnimation = {
+  dark: css`
+    scale: 40;
+    transition: scale 1000ms ease-in;
+  `,
+  light: css`
+    scale: 0;
+    transition: scale 800ms ease-out;
+  `,
+};
+
+export const ThemeCircle = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  translate: -50% -50%;
+  background-color: hsla(247, 21%, 8%, 1);
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  transform-origin: center center;
+  ${({ themeType }) => ThemeAnimation[themeType]}
+  z-index: -1;
 `;
